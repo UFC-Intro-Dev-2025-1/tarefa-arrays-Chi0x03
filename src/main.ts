16bce0b2115c121c8ecc5996856c38b1492c1d33
Caio Wagner <caiowagner@alu.ufc.br>
2025-07-10 19:08:03 +0000

    feat: solução da tarefa - atividade main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,25 +19,26 @@ const numeros: number[] = [10, 20, 30, 40, 50];
 
 function mostrarArray(array: number[]): void {
     console.log('--------------------------------');
-    console.log(`Tamanho: `);
-    console.log(`Array: `);
+    console.log(`Tamanho: ${array.length}`);
+    console.log(`Array: ${array}`);
 }
+
+numeros.push(60);
+numeros.push(70);
 mostrarArray(numeros);
 
-
-
-
-
-const elementoExcluido = ;
+const elementoExcluido = numeros.pop();
 mostrarArray(numeros);
 console.log(`Elemento excluído: ${elementoExcluido}`);
 
-numeros.forEach();
+numeros.forEach(function (item) {
+    console.log(item);
+});
 
-const dobrados = numeros.map();
+const dobrados = numeros.map((item) => item * 2);
 mostrarArray(dobrados);
 
-const soma = numeros.reduce();
+const soma = numeros.reduce((acc, value) => acc + value);
 console.log(`Soma dos elementos do array: ${soma}`);
 
 // para rodar o código, use o comando:
